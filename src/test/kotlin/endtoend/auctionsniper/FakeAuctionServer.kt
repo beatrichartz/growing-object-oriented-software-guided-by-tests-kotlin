9e801f1f06365e064dda8b1d85a4eaa5269326ac
Beat Richartz <me@beatrichartz.com>
2019-04-14 11:31:04 +1000

    [RED] Moved fake auction server item id field to more idiomatic instantiation

--- src/test/kotlin/endtoend/auctionsniper/FakeAuctionServer.kt
+++ src/test/kotlin/endtoend/auctionsniper/FakeAuctionServer.kt
@@ -13,7 +13,7 @@ import java.lang.String.format
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.TimeUnit
 
-class FakeAuctionServer(itemId: String) {
+class FakeAuctionServer(internal val itemId: String) {
     companion object {
         const val ITEM_ID_AS_LOGIN = "auction-%s"
         const val AUCTION_RESOURCE = "Auction"
@@ -21,7 +21,6 @@ class FakeAuctionServer(itemId: String) {
         const val AUCTION_PASSWORD = "auction"
     }
 
-    internal val itemId: String = itemId
     private val connection: XMPPConnection
     private val messageListener: SingleMessageListener = SingleMessageListener()
     private var currentChat: Chat? = null
