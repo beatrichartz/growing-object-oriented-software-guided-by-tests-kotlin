af2f3ea42a3ef993cc539040cba315bbe6bf8af5
Beat Richartz <me@beatrichartz.com>
2019-06-12 18:52:23 +1000

    [RED] Expected a table in sniper UI

--- src/test/kotlin/endtoend/auctionsniper/AuctionSniperDriver.kt
+++ src/test/kotlin/endtoend/auctionsniper/AuctionSniperDriver.kt
@@ -4,9 +4,10 @@ import auctionsniper.MainWindow
 import com.objogate.wl.swing.AWTEventQueueProber
 import com.objogate.wl.swing.driver.ComponentDriver
 import com.objogate.wl.swing.driver.JFrameDriver
-import com.objogate.wl.swing.driver.JLabelDriver
+import com.objogate.wl.swing.driver.JTableDriver
 import com.objogate.wl.swing.gesture.GesturePerformer
-import org.hamcrest.CoreMatchers
+import com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText
+import org.hamcrest.CoreMatchers.equalTo
 
 class AuctionSniperDriver(timeoutMillis: Long): JFrameDriver(
         GesturePerformer(),
@@ -14,6 +15,6 @@ class AuctionSniperDriver(timeoutMillis: Long): JFrameDriver(
         AWTEventQueueProber(timeoutMillis, 100)
 ) {
     fun showsSniperStatus(statusText: String) {
-        JLabelDriver(this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(CoreMatchers.equalTo(statusText))
+        JTableDriver(this).hasCell(withLabelText(equalTo(statusText)))
     }
 }
